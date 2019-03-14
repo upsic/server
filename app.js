78b4e4de5a656dc2a2aae84aae8c3dc57c2d94c5
willyprayogo26 <willyprayogo26@gmail.com>
2019-03-14 18:30:47 +0700

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+const port = process.env.PORT || 3000
+const routes = require('./routes')
+
+mongoose.connect('mongodb://localhost:27017/upsic', { useNewUrlParser: true })
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', routes)
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
