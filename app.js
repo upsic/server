5afab3143a1e356867fb9892796313fc879d2917
shadowM8 <anton2014lenovo@gmail.com>
2019-03-14 19:59:41 +0700

    add router/music.js; add controller/music.js; add models/Music.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes')
+const musicRoute = require('./routes/music')
 
 mongoose.connect('mongodb://localhost:27017/upsic', { useNewUrlParser: true })
 
@@ -13,7 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/upsic', routes)
+app.use('/', routes)
+app.use('/music', musicRoute)
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
