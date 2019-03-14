6a55689e4ccb531a35513f4a216858f735f78f87
shadowM8 <anton2014lenovo@gmail.com>
2019-03-14 21:08:14 +0700

    branch upload rebase ke dev

--- models/music.js
+++ models/music.js
@@ -5,10 +5,13 @@ const musicSchema = new Schema({
     title : String,
     artist : String,
     url : String,
-    img_url : String,
+    img_url : {
+        type : String,
+        default : `https://iweek.org.za/wp-content/uploads/2015/09/no-profile-photo1.jpg`
+    },
     user : {
         type: Schema.Types.ObjectId,
-        ref : `user`
+        ref : `User`
     }
 })
 
